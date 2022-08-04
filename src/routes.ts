4204e917b4317c0b167332d04b5a1e752d6bccdb
Leonardo Bellei <79417128+L-Bellei@users.noreply.github.com>
2022-08-04 09:09:58 -0400

    listando produtos por categoria

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from './config/multer';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from "./controllers/user/DetailUserController";
@@ -6,8 +8,7 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { CreateProductController } from './controllers/product/CreateProductController';
-import multer from "multer";
-import uploadConfig from './config/multer';
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 const router = Router();
 const upload = multer(uploadConfig.upload("./tmp"));
@@ -23,5 +24,6 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 //Rotas de Produto
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
 
 export { router };
